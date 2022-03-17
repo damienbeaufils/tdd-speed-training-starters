7cc9b4ca13e6b4107698f192db8a90daa741a90b
Damien Beaufils <damien.beaufils@gmail.com>
2022-03-17 09:21:16 -0400

    ✨ add typescript starter
    see https://github.com/jsynowiec/node-typescript-boilerplate

---
+++ typescript/__tests__/main.test.ts
@@ -0,0 +1,14 @@
+describe('main', () => {
+  describe('hello()', () => {
+    it('should do or return this when that', () => {
+      // given
+      const expected = true;
+
+      // when
+      const result = false;
+
+      // then
+      expect(result).toBe(expected);
+    });
+  });
+});
