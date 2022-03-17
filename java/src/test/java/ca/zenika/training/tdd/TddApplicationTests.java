ed5a7ca0f5182a84c5f3941f93cbce9712823088
Damien Beaufils <damien.beaufils@gmail.com>
2022-03-17 09:12:13 -0400

    🎉 Initial commit with Java starter using JDK17

---
+++ java/src/test/java/ca/zenika/training/tdd/TddApplicationTests.java
@@ -0,0 +1,18 @@
+package ca.zenika.training.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TddApplicationTests {
+
+    @Test
+    void name_should_return_or_do_this_when_that() {
+        // given
+
+        // when
+
+        // then
+        assertThat(false).isTrue();
+    }
+}
